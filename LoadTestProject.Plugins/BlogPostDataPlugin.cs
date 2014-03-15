b77d187cc5d81c8749eaeab6437b7db191e62ed3
n33470 <n33470@gmail.com>
2014-03-15 12:45:25 -0500

    Updates

--- LoadTestProject.Plugins/BlogPostDataPlugin.cs
+++ LoadTestProject.Plugins/BlogPostDataPlugin.cs
@@ -19,37 +19,49 @@ namespace LoadTestProject.Plugins
         [DefaultValue("ContentField")]
         public string ContentParameterName { get; set; }
 
-        public override void PreWebTest(object sender, PreWebTestEventArgs e)
-        {
-            base.PreWebTest(sender, e);
-        }
-
-        public override void PrePage(object sender, PrePageEventArgs e)
-        {
-            base.PrePage(sender, e);
-        }
-
         public override void PreRequest(object sender, PreRequestEventArgs e)
         {
+            // When overridden in a derived class, represents the method 
+            // that will handle the event associated with the start of an HTTP request.
             base.PreRequest(sender, e);
 
-            // the TestNumber parameter comes from the MyTestPlugin which is connected to the CreatePost.loadtest
+            // the TestNumber parameter comes from the MyTestPlugin 
+            // which is connected to the CreatePost.loadtest
             var testNumber = -1;
             if(e.WebTest.Context.ContainsKey("testNumber"))
                 testNumber = (int)e.WebTest.Context["testNumber"];
 
-            // the PostId parameter comes from the extraction rule contained in the EditPost.webtest
+            // the PostId parameter comes from the extraction rule 
+            // contained in the EditPost.webtest
             string postId = "";
             if (e.WebTest.Context.ContainsKey("PostId"))
                 postId = e.WebTest.Context["PostId"].ToString();
 
 
             // Content field will have TestNumber, PostId, timestamp, and new GUID
-            e.WebTest.Context[this.ContentParameterName] = string.Format("TestNumber: {0} postId: {1} at: {2} id: {3}", testNumber, postId, DateTime.Now, Guid.NewGuid());
+            e.WebTest.Context[this.ContentParameterName] = 
+                string.Format("TestNumber: {0} postId: {1} at: {2} id: {3}", 
+                testNumber, postId, DateTime.Now, Guid.NewGuid());
+
 
             // Title field will be random number between 0-100
             var rand = new Random();
-            e.WebTest.Context[this.TitleParameterName] = string.Format("Random Number: {0}", rand.Next(101));
+            e.WebTest.Context[this.TitleParameterName] = 
+                string.Format("Random Number: {0}", rand.Next(101));
+        }
+
+        public override void PreWebTest(object sender, PreWebTestEventArgs e)
+        {
+            // When overridden in a derived class, represents the method 
+            // that will handle the event associated with the start of a Web performance test.
+            base.PreWebTest(sender, e);
+        }
+
+        public override void PrePage(object sender, PrePageEventArgs e)
+        {
+            // When overridden in a derived class, represents the method 
+            // that will handle the event associated with the start of a Web page.
+            base.PrePage(sender, e);
         }
     }
 }
